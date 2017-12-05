163e8a866882538a8ff7b6b51d5e0e20b61820f4
Jerry Chai <jerry@gooddogdesign.com>
2017-12-05 00:56:17 -0800

    migrate send token contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
 var Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
-var Authentication = artifacts.require("./Authentication.sol");
+var SendToken = artifacts.require("./SendToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Ownable);
   deployer.link(Ownable, Killable);
   deployer.deploy(Killable);
-  deployer.link(Killable, Authentication);
-  deployer.deploy(Authentication);
+  deployer.link(Killable, SendToken);
+  deployer.deploy(SendToken);
 };
